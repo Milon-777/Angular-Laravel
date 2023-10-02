00fbfef367862e5d67242e1f8d020666d4a9cb00
Strizhak Roman <milon777.official@gmail.com>
2023-10-02 12:15:31 +0300

    made changes to text and styles

--- backend/database/migrations/2023_09_22_084328_create_articles_table.php
+++ backend/database/migrations/2023_09_22_084328_create_articles_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
-            $table->string('content');
+            $table->longText('content');
             $table->string('author');
             $table->timestamps();
         });
